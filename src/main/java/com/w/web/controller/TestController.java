872c1173d89611bbf42631c21e56579b2765cb13
dev2 <dev2@macair.com>
2025-11-18 16:52:12 +0800

    1

--- src/main/java/com/w/web/controller/TestController.java
+++ src/main/java/com/w/web/controller/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Map;
+
 @RestController
 public class TestController {
 
@@ -20,6 +22,19 @@ public class TestController {
                 .retrieve()
                 .toBodilessEntity()
                 .block();
+
+        String result = webClient.post()
+                .uri("https://example.com/api/login")
+                .header("User-Agent", "Mozilla/5.0")
+                .header("x-api-key", "123456")
+                .bodyValue(Map.of(
+                        "username", "will",
+                        "password", "123456"
+                ))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
 
     }
 }
